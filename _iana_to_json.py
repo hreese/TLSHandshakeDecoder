cc7634965981e4034cebe1812ce65c7022e4de9f
Heiko Reese <mail@heiko-reese.de>
2014-11-26 21:36:03 +0100

    converted list of cipher suits to go literals

--- _iana_to_json.py
+++ _iana_to_json.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import csv
 import json
 
@@ -10,6 +12,20 @@ with open("iana_tls-params_min.csv") as f:
         ciphers.append( { 'index': idx, 'value': value, 'name': row[2]} )
         idx += 1
 
-with open("iana_tls-params_min.json", "w") as outf:
-    json.dump(ciphers, outf, indent=2)
+# write json
+#with open("iana_tls-params_min.json", "w") as outf:
+#    json.dump(ciphers, outf, indent=2)
 
+print("// all IANA registered cyphers, indexed by position in list")
+print("var CiphersByIndex map[uint]CipherSuite = map[uint]CipherSuite{")
+for c in ciphers:
+    print( "  %d: CipherSuite{Index: %d, Value: %d, Name: \"%s\"}," % (c['index'], c['index'], c['value'], c['name']) )
+
+print("}\n")
+
+print("// all IANA registered cyphers, indexed by identifier")
+print("var CiphersByValue map[uint]CipherSuite = map[uint]CipherSuite{")
+for c in ciphers:
+    print( "  %d: CipherSuite{Index: %d, Value: %d, Name: \"%s\"}," % (c['value'], c['index'], c['value'], c['name']) )
+
+print("}\n")
